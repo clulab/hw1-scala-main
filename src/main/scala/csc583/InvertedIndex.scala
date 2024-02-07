648d1006a8dbe8e0c9aeb5b6ccb6c71c5cfd0ab6
Mihai Surdeanu <surdeanu@gmail.com>
2024-02-07 08:24:33 -0700

    Something

--- src/main/scala/csc583/InvertedIndex.scala
+++ src/main/scala/csc583/InvertedIndex.scala
@@ -2,7 +2,7 @@ package csc583
 
 class InvertedIndex(val inputFile: String) {
 	def runQ5_1(query: String): Seq[String] = {
-		List() // return an empty list by default
+		List("Doc1") // return an empty list by default
 	}
 
 	def runQ5_2(query: String): Seq[String] = {
